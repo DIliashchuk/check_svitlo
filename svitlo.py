f6a691e2aefa4e89718eca9c7cc1ed83ebecd27e
Dmitriy Iliashchuk <diliashchuk@gmail.com>
2024-07-10 16:47:04 +0300

    Update svitlo.py

--- svitlo.py
+++ svitlo.py
@@ -1,29 +1,32 @@
 import streamlit as st
-import subprocess
+from scapy.all import *
+
 
 def icmp_ping(host):
     try:
-        # Виконання системної команди ping з повним шляхом
-        result = subprocess.run(['/bin/ping', '-c', '4', host], capture_output=True, text=True)
-        
-        if result.returncode == 0:
-            # Виведення результату пінгу
-            return f"Урааа іди включай сімс:\n{result.stdout}"
+        # Виконання пінгу за допомогою ICMP пакетів з допомогою scapy
+        packet = IP(dst=host) / ICMP()
+        reply = sr1(packet, timeout=2, verbose=False)
+
+        if reply:
+            return f"Урааа іди включай сімс: {host} доступний!"
         else:
-            return f"От блєт, читай книжечку і чекай. Could not reach '{host}':\n{result.stderr}"
+            return f"От блєт, читай книжечку і чекай. Could not reach '{host}'"
     except Exception as e:
         return f"Error: {str(e)}"
+
 
 def main():
     st.title('ICMP Ping Checker')
 
-    host = st.text_input('Пиши IP:', key='host_input')
+    host = st.text_input('Пиши IP:')
 
-    if st.button('Ping', key='ping_button'):
+    if st.button('Ping'):
         if host:
             st.write(f'Pinging {host}...')
             result = icmp_ping(host)
             st.write(result)
+
 
 if __name__ == '__main__':
     main()
