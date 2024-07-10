9adb04ce002b2738d076fb3a59fea5232d1c52b3
Dmitriy Iliashchuk <diliashchuk@gmail.com>
2024-07-10 14:06:59 +0300

    Update svitlo.py

--- svitlo.py
+++ svitlo.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import requests
 
-def is_router_accessible(router_ip):
+def is_router_accessible(public_ip):
     try:
-        response = requests.get(f'http://{router_ip}', timeout=5)
+        response = requests.get(f'http://{public_ip}', timeout=5)
         return response.status_code == 200
     except requests.ConnectionError:
         return False
 
 st.title("Check Home Power Status")
 
-router_ip = st.text_input("Enter the IP address of your router:", "192.168.0.1")
+public_ip = st.text_input("Enter the public IP address of your router:", "YOUR_PUBLIC_IP")
 
 if st.button("Check Light"):
-    if is_router_accessible(router_ip):
+    if is_router_accessible(public_ip):
         st.success("Світло є вдома.")
     else:
         st.error("Світла немає вдома.")
