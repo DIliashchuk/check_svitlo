3702344f5d67c91e50db9a9e8b5bf3a93e9f1b0e
Dmitriy Iliashchuk <diliashchuk@gmail.com>
2024-07-10 15:20:24 +0300

    Update svitlo.py

--- svitlo.py
+++ svitlo.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import requests
+import subprocess
+
 
 def is_router_accessible(public_ip):
     try:
@@ -8,12 +10,29 @@ def is_router_accessible(public_ip):
     except requests.ConnectionError:
         return False
 
+
+def ping_ip_address(ip_address):
+    try:
+        subprocess.check_call(["ping", "-c", "1", ip_address])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
 st.title("Check Home Power Status")
 
-public_ip = st.text_input("Enter the public IP address of your router!:", "YOUR_PUBLIC_IP")
+public_ip = st.text_input("Enter the public IP address of your router:", "YOUR_PUBLIC_IP")
+ip_to_ping = st.text_input("Enter the IP address to ping:", "8.8.8.8")  # Default to Google's DNS server
 
 if st.button("Check Light"):
-    if is_router_accessible(public_ip):
+    router_accessible = is_router_accessible(public_ip)
+    if router_accessible:
         st.success("Світло є вдома.")
     else:
         st.error("Світла немає вдома.")
+
+    ip_pingable = ping_ip_address(ip_to_ping)
+    if ip_pingable:
+        st.success(f"Ping to {ip_to_ping} successful.")
+    else:
+        st.error(f"Ping to {ip_to_ping} unsuccessful.")
