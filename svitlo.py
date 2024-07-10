adc9cf6f0a2afa9cf7931a3e4df7c11cd55f9efb
Dmitriy Iliashchuk <diliashchuk@gmail.com>
2024-07-10 16:43:36 +0300

    Update svitlo.py

--- svitlo.py
+++ svitlo.py
@@ -1,39 +1,25 @@
 import streamlit as st
 import subprocess
 
-
 def icmp_ping(host):
     try:
-
+        # Виконання системної команди ping
         result = subprocess.run(['ping', '-c', '4', host], capture_output=True, text=True)
-
+        
         if result.returncode == 0:
-
+            # Виведення результату пінгу
             return f"Урааа іди включай сімс:\n{result.stdout}"
         else:
             return f"От блєт, читай книжечку і чекай. Could not reach '{host}':\n{result.stderr}"
     except Exception as e:
         return f"Error: {str(e)}"
 
-
 def main():
-    st.title('Перевірка Світла')
+    st.title('ICMP Ping Checker')
 
-    host = st.text_input('Пиши IP:')
+    host = st.text_input('Пиши IP:', key='host_input')
 
-    if st.button('Ping'):
-        if host:
-            st.write(f'Pinging {host}...')
-            result = icmp_ping(host)
-            st.write(result)
-
-
-if __name__ == '__main__':
-    main()
-
-    host = st.text_input('Пиши IP:')
-
-    if st.button('Ping'):
+    if st.button('Ping', key='ping_button'):
         if host:
             st.write(f'Pinging {host}...')
             result = icmp_ping(host)
