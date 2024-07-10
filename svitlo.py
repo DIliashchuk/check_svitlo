d2611642f69ef79742796a0c48bfc601b13a8beb
Dmitriy Iliashchuk <diliashchuk@gmail.com>
2024-07-10 16:32:58 +0300

    Update svitlo.py

--- svitlo.py
+++ svitlo.py
@@ -1,26 +1,30 @@
 import streamlit as st
-from ping3 import ping, verbose_ping
+from pythonping import ping
+
 
 def icmp_ping(host):
     try:
-        result = ping(host, unit='ms')
-        if result is not None:
-            return f"Ping successful. Round-trip time: {result:.2f} ms"
+        response_list = ping(host, count=4)
+        if response_list.success():
+            avg_rtt = sum([r.time_elapsed_ms for r in response_list]) / len(response_list)
+            return f"Урааа іди включай сімс: {avg_rtt:.2f} ms"
         else:
-            return f"Ping failed. Could not reach '{host}'"
+            return f"От блєт, читай книжечку і чекай. Could not reach '{host}'"
     except Exception as e:
         return f"Error: {str(e)}"
 
+
 def main():
     st.title('ICMP Ping Checker')
-    
-    host = st.text_input('Enter IP address or hostname:')
-    
+
+    host = st.text_input('Пиши IP:')
+
     if st.button('Ping'):
         if host:
             st.write(f'Pinging {host}...')
             result = icmp_ping(host)
             st.write(result)
+
 
 if __name__ == '__main__':
     main()
